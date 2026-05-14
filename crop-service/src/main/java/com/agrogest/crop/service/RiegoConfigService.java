8b5e42e4783382ff43165737bd9c63517799797a
Marco Infante Agurto <marco@agrogest.com>
2026-05-13 23:30:51 -0500

    refactor: move notification-service to project root - clean microservice structure

--- crop-service/src/main/java/com/agrogest/crop/service/RiegoConfigService.java
+++
@@ -1,9 +0,0 @@
-package com.agrogest.crop.service;
-
-import com.agrogest.crop.dto.*;
-import java.util.UUID;
-
-public interface RiegoConfigService {
-    RiegoConfigResponse getBySiembra(UUID siembraId);
-    RiegoConfigResponse update(UUID siembraId, UpdateRiegoConfigRequest request);
-}
