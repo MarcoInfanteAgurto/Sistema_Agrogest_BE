cf4039e7df2727841b79fcd730414987683d7138
FabrizioRam <fabrizio.ramirez.sanchez@vallegrande.edu.pe>
2026-05-21 20:14:00 -0500

    feature: fix usuariocreadorconsumer

--- src/main/java/com/agrogest/notification/kafka/consumers/UsuarioCreadoConsumer.java
+++ src/main/java/com/agrogest/notification/kafka/consumers/UsuarioCreadoConsumer.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class NotificacionConsumer {
+public class UsuarioCreadoConsumer {
 
     private final NotificacionRepository notificationRepository;
     private final ObjectMapper objectMapper;
